221570f6c66d8998475818a90c500423449c7ee5
paulj3 <john.paul@vumc.org>
2018-03-28 08:30:12 -0500

    create bundle file

---
+++ VictrOncoreApiBundle.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *  Copyright 2018 Vanderbilt University Medical Center
+ */
+namespace Victr\OncoreApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class VictrOncoreApiBundle extends Bundle
+{
+
+}
